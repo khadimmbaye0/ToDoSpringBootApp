50a200c12f22bffd75fd10e9647c23ffbfa37113
khadimmbaye0 <khadimmbaye4002@gmail.com>
2025-07-20 18:28:45 +0000

    Initial commit [-_-]

---
+++ src/main/java/com/firstappwithspringboot/todospringbootapp/ToDoSpringBootAppApplication.java
@@ -0,0 +1,13 @@
+package com.firstappwithspringboot.todospringbootapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToDoSpringBootAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ToDoSpringBootAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/firstappwithspringboot/todospringbootapp/ToDoSpringBootAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.firstappwithspringboot.todospringbootapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToDoSpringBootAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
